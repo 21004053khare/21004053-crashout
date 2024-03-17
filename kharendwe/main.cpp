34ada0b38f2d4838e34b85472501b99eabd4caad
21004053khare <163664223+21004053khare@users.noreply.github.com>
2024-03-17 20:23:04 +0200

    Add files via upload

---
+++ kharendwe/main.cpp
@@ -0,0 +1,27 @@
+#include <cmath>
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+
+
+ int dis;
+  int heigt;
+   int angle;
+    int out;
+    cout<<"enter height in inches"<<endl;
+     cin>>hegt;
+     cout<<"enter distance in feet"<<endl;
+      cin>>dis; cout<<"ente angle in degrees"<<endl;
+       cin>>angle;
+       out=((h0.883)+(dtan(angle)));
+       cout<<"answer you looking for is:"<<output<<endl;
+
+    return 0;
+}
